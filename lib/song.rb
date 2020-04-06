6980848361ec650bfcf06ef1fb53fc613988a2c7
Heather Moreira <moreiraheather@gmail.com>
2020-04-06 20:49:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   
   def self.create 
     song = Song.new 
-    song.save 
+    song.save     
     song 
   end 
   
@@ -62,8 +62,8 @@ class Song
   
 
   def self.create_from_filename(filename)
-    self.new_from_filename(filename)    
-    binding.pry
+    song = self.new_from_filename(filename)    
+    
   end 
   
   def self.destroy_all
